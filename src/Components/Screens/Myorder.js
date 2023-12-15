0172aa69c425dc6586d5660c890d65fe197a2fcd
Piyush <HP@DESKTOP-JFKIEAH.>
2023-12-15 12:59:01 +0530

    final update

---
+++ src/Components/Screens/Myorder.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+// import Footer from '../components/Footer';
+import Footer from '../Footer';
+// import Navbar from '../components/Navbar';
+
+import Navbar from '../Navbar';
+export default function MyOrder() {
+    
+const apiURL = process.env.REACT_APP_BASE_URL;
+
+    const [orderData, setorderData] = useState({})
+
+    const fetchMyOrder = async () => {
+        console.log(localStorage.getItem('email'))
+        
+        await fetch(apiURL+"/api/myorderData", {
+            // credentials: 'include',
+            // Origin:"http://localhost:3000/login",
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                email:localStorage.getItem('email')
+            })
+        }).then(async (res) => {
+            let response = await res.json()
+            await setorderData(response)
+            console.log(response)
+        })
+
+
+
+        // await res.map((data)=>{
+        //    console.log(data)
+        // })
+
+
+    }
+
+    useEffect(() => {
+        fetchMyOrder()
+    }, [])
+
+    return (
+        <div>
+            <div>
+                <Navbar />
+            </div>
+
+            
+
+            <Footer />
+        </div>
+    )
+}
